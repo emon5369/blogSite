16e68f2edb440a9b4e340ef8b507e33558d22ea9
emon5369 <emon5369@gmail.com>
2024-08-26 13:51:02 +0600

    latest update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,7 +61,7 @@ function Header() {
                                 item.active ? (
                                     <li key={item.name} className="ml-2 text-base font-semibold">
                                         <button onClick={() => navigate(item.slug)}
-                                            className="inline-block px-6 py-2 duration-200 text-gray-600 hover:bg-blue-200 rounded-full">
+                                            className="inline-block px-6 py-2 duration-200 text-gray-600 hover:bg-slate-300 rounded-full">
                                             {item.name}
                                         </button>
                                     </li>
